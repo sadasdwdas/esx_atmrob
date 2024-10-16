e2eea854fe6d3e461ff0cdc34e692c9695db47e7
Maistelija <141753153+sadasdwdas@users.noreply.github.com>
2024-10-16 19:09:35 +0300

    config.lua

--- esx_atmrob/config.lua
+++ esx_atmrob/config.lua
@@ -1,18 +1,21 @@
 Config = {}
 
-Config.ATMProps = {
+Config.AutomaattiPropit = {
     'prop_atm_01',
     'prop_atm_02',
     'prop_atm_03'
 }
 
-Config.ATMCooldown = 21600
+Config.AutomaattiCooldown = 21600
 
-Config.Reward = {
-    min = 500,
-    max = 2000
+Config.Palkkiot = {
+    {item = 'money', itemlabel = 'Käteinen', maxMaara = 2000, minMaara = 500, chance = 100}, 
+    {item = 'black_money', itemlabel = 'Likainen raha', maxMaara = 22000, minMaara = 2500, chance = 50}  
 }
 
-Config.ShockChance = 20
 
-Config.ShockDuration = 30
+Config.IskuMahdollisuus = 30
+Config.IskunKesto = 30
+Config.RyostaTapa = 'textui' -- 'target', 'textui'
+Config.PoliisinIlmoitus = 'cd' -- 'cd', 'normi'
+Config.PoliisinBlipKesto = 60 -- vain normi ilmotukseen
